101e34c0c31aa0c98166b52f416d62778c32bdfb
unipeak99 <unipeak99@gmail.com>
2018-06-10 19:20:46 +0000

    add all files

---
+++ www/restapi/routes/prt.py
@@ -0,0 +1,10 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+
+opts = FirefoxOptions()
+opts.add_argument("--headless")
+
+driver  = webdriver.Firefox(firefox_options=opts)
+driver.get(str(sys.argv[1]));
+driver.get_screenshot_as_file('/home/njtu1133/factom-db/public/'+str(sys.argv[2]) +'.png')
